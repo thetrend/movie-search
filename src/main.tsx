af5b7de71d2314f6ff958c6a373186922fa6d928
Grace de la Mora <thetrend@users.noreply.github.com>
2025-01-13 23:22:21 -0500

    feat: main page first pass

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faBars,
+  faList,
+  faMagnifyingGlass,
+  faTableCellsLarge,
+} from '@fortawesome/free-solid-svg-icons'
 import App from './App.tsx'
+
+library.add(faBars, faList, faMagnifyingGlass, faTableCellsLarge)
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
